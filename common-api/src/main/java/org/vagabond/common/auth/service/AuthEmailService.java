c3577c08bb47fc44e7b2b8b56a196babff5a899b
GCL <gonzague.clement@gmail.com>
2024-03-03 02:26:41 +0100

    fix: email service with good website name

--- common-api/src/main/java/org/vagabond/common/auth/service/AuthEmailService.java
+++ common-api/src/main/java/org/vagabond/common/auth/service/AuthEmailService.java
@@ -1,5 +1,8 @@
 package org.vagabond.common.auth.service;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.vagabond.common.user.UserEntity;
 import org.vagabond.engine.email.builder.EmailBuilder;
@@ -7,58 +10,46 @@ import org.vagabond.engine.email.dto.ResponseBodyDto;
 import org.vagabond.engine.email.kafka.payload.EmailKafkaRequest;
 import org.vagabond.engine.email.kafka.service.EmailKafkaService;
 
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-
 @ApplicationScoped
 public class AuthEmailService {
 
-        private static final String WEBSITE_NAME = "${WEBSITE_NAME}";
-        private static final String WEBSITE_URL = "${WEBSITE_URL}";
+    private static final String WEBSITE_NAME = "${WEBSITE_NAME}";
+    private static final String WEBSITE_URL = "${WEBSITE_URL}";
 
-        @ConfigProperty(name = "website.name")
-        private String websiteName;
-        @ConfigProperty(name = "website.url")
-        private String websiteUrl;
-        @ConfigProperty(name = "website.url.activation")
-        private String websiteUrlActivation;
+    @ConfigProperty(name = "website.name")
+    private String websiteName;
+    @ConfigProperty(name = "website.url")
+    private String websiteUrl;
+    @ConfigProperty(name = "website.url.activation")
+    private String websiteUrlActivation;
 
-        @Inject
-        private EmailKafkaService emailKafkaService;
+    @Inject
+    private EmailKafkaService emailKafkaService;
 
-        public void sendCreationMail(UserEntity user) {
-                ResponseBodyDto emailBody = new EmailBuilder("/email/create-user.html")
-                                .addSelector("${LOGIN}", user.username)
-                                .addSelector(WEBSITE_NAME, websiteName)
-                                .addSelector(WEBSITE_URL, websiteUrl)
-                                .addSelector("${WEBSITE_ACTION}",
-                                                websiteUrlActivation + user.activationToken)
-                                .build();
+    public void sendCreationMail(UserEntity user) {
+        ResponseBodyDto emailBody = new EmailBuilder("/email/create-user.html").addSelector("${LOGIN}", user.username)
+                .addSelector(WEBSITE_NAME, websiteName).addSelector(WEBSITE_URL, websiteUrl)
+                .addSelector("${WEBSITE_ACTION}", websiteUrlActivation + user.activationToken).build();
 
-                emailKafkaService.sendEmailOutgoing(new EmailKafkaRequest(user.email,
-                                "Bingo Dingo - Activer votre compte", emailBody.content()));
-        }
+        emailKafkaService
+                .sendEmailOutgoing(new EmailKafkaRequest(user.email, websiteName + " - Activer votre compte", emailBody.content()));
+    }
 
-        public void sendIdentityTokenMail(UserEntity user) {
+    public void sendIdentityTokenMail(UserEntity user) {
 
-                ResponseBodyDto emailBody = new EmailBuilder("/email/identity-token.html")
-                                .addSelector(WEBSITE_NAME, websiteName)
-                                .addSelector(WEBSITE_URL, websiteUrl)
-                                .addSelector("${IDENTITY_TOKEN}", user.identityToken).build();
+        ResponseBodyDto emailBody = new EmailBuilder("/email/identity-token.html").addSelector(WEBSITE_NAME, websiteName)
+                .addSelector(WEBSITE_URL, websiteUrl).addSelector("${IDENTITY_TOKEN}", user.identityToken).build();
 
-                emailKafkaService.sendEmailOutgoing(new EmailKafkaRequest(user.email,
-                                "Bingo Dingo - Vérification de votre identité",
-                                emailBody.content()));
-        }
+        emailKafkaService.sendEmailOutgoing(
+                new EmailKafkaRequest(user.email, websiteName + " - Vérification de votre identité", emailBody.content()));
+    }
 
-        public void sendResetPassword(UserEntity user, String newPassword) {
+    public void sendResetPassword(UserEntity user, String newPassword) {
 
-                ResponseBodyDto emailBody = new EmailBuilder("/email/reset-password.html")
-                                .addSelector(WEBSITE_NAME, websiteName)
-                                .addSelector(WEBSITE_URL, websiteUrl)
-                                .addSelector("${NEW_PASSWORD}", newPassword).build();
+        ResponseBodyDto emailBody = new EmailBuilder("/email/reset-password.html").addSelector(WEBSITE_NAME, websiteName)
+                .addSelector(WEBSITE_URL, websiteUrl).addSelector("${NEW_PASSWORD}", newPassword).build();
 
-                emailKafkaService.sendEmailOutgoing(new EmailKafkaRequest(user.email,
-                                "Bingo Dingo - Nouveau mot de passe", emailBody.content()));
-        }
+        emailKafkaService
+                .sendEmailOutgoing(new EmailKafkaRequest(user.email, websiteName + " - Nouveau mot de passe", emailBody.content()));
+    }
 }
