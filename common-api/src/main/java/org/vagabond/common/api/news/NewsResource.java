0fce93595826c961a389dde4e73079f99cab94dc
GCL <gonzague.clement@gmail.com>
2025-05-25 20:09:05 +0200

    fix: remove overhide findAll with admin role

--- common-api/src/main/java/org/vagabond/common/api/news/NewsResource.java
+++ common-api/src/main/java/org/vagabond/common/api/news/NewsResource.java
@@ -3,15 +3,11 @@ package org.vagabond.common.api.news;
 import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.QueryParam;
-import jakarta.ws.rs.core.Response;
 
 import org.vagabond.common.news.NewsEntity;
 import org.vagabond.common.news.NewsService;
 import org.vagabond.common.news.payload.NewsResponse;
 import org.vagabond.common.user.UserEntity;
-import org.vagabond.engine.auth.annotation.AuthRole;
-import org.vagabond.engine.auth.annotation.AuthSecure;
 import org.vagabond.engine.crud.resource.BaseCrudResource;
 
 import io.smallrye.common.annotation.RunOnVirtualThread;
@@ -27,12 +23,5 @@ public class NewsResource extends BaseCrudResource<NewsEntity, UserEntity> {
     public void postConstruct() {
         service = newsService;
         responseClass = NewsResponse.class;
-    }
-
-    @AuthSecure
-    @AuthRole("ADMIN")
-    @Override
-    public Response findAll(@QueryParam("page") Integer page, @QueryParam("max") Integer max, @QueryParam("sort") String sort) {
-        return super.findAll(page, max, sort);
     }
 }
